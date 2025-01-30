c5215f563f88d3a38bd211efbf494b5343802eb3
DieterVanhove <dieter.vanhove@outlook.com>
2025-01-30 09:01:08 +0100

    Update database configuration to use App::env() method consistently

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -3,13 +3,13 @@
 use craft\helpers\App;
 
 return [
-    'dsn' => getenv('DB_DSN') ?: 'mysql:host=127.0.0.1;port=3306;dbname=craft_test',
+    'dsn' => App::env('DB_DSN') ?: null,
     'driver' => App::env('DB_DRIVER'),
     'server' => App::env('DB_SERVER'),
     'port' => App::env('DB_PORT'),
     'database' => App::env('DB_DATABASE'),
-    'user' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
-    'schema' => getenv('DB_SCHEMA') ?: 'public',
-    'tablePrefix' => getenv('DB_TABLE_PREFIX') ?: '',
+    'user' => App::env('DB_USER'),
+    'password' => App::env('DB_PASSWORD'),
+    'schema' => App::env('DB_SCHEMA'),
+    'tablePrefix' => App::env('DB_TABLE_PREFIX'),
 ];
